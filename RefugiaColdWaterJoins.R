cddb806089d5864ebf461b999937b983bc5f5992
dmaxsmith <oregonmax@gmail.com>
2025-09-01 12:32:08 -0600

    Add files via upload

---
+++ RefugiaColdWaterJoins.R
@@ -0,0 +1,180 @@
+setwd("E:/2023_CurrentNM_Work/R/Tables")
+
+library(tidyverse)
+
+#Value#
+colwatjoin1<-full_join(colperhuc12s, HUC12fishes, "HUC12")
+colwatjoin1_noNAs<-na.omit(colwatjoin1)
+
+colwatjoin2<-full_join(colwatjoin1_noNAs, huc12sprden, "HUC12")
+colwatjoin2_noNAs<-na.omit(colwatjoin2)
+
+colwatjoin3<-full_join(colwatjoin2_noNAs, huc12pct10deg, "HUC12")
+colwatjoin3_noNAs<-na.omit(colwatjoin3)
+
+##Climate/hydrology##
+colwatjoin4<-full_join(colwatjoin3_noNAs, huc12diftemwetqua, "HUC12") #Bio8
+colwatjoin4_noNAs<-na.omit(colwatjoin4)
+
+colwatjoin5<-full_join(colwatjoin4_noNAs, huc12diftemdriqua, "HUC12") #Bio9
+colwatjoin5_noNAs<-na.omit(colwatjoin5)
+
+colwatjoin6<-full_join(colwatjoin5_noNAs, huc12difmaxtemwarmon, "HUC12") #Bio5
+colwatjoin6_noNAs<-na.omit(colwatjoin6)
+
+colwatjoin7<-full_join(colwatjoin6_noNAs, huc12difanntem, "HUC12") #Bio1
+colwatjoin7_noNAs<-na.omit(colwatjoin7)
+
+colwatjoin8<-full_join(colwatjoin7_noNAs, huc12pctchaprewarqua, "HUC12") #Bio18
+colwatjoin8_noNAs<-na.omit(colwatjoin8)
+
+colwatjoin9<-full_join(colwatjoin8_noNAs, huc12perchaprecolqua, "HUC12") #Bio19
+colwatjoin9_noNAs<-na.omit(colwatjoin9)
+
+colwatjoin10<-full_join(colwatjoin9_noNAs, huc12perchaannpre, "HUC12") #Bio12
+colwatjoin10_noNAs<-na.omit(colwatjoin10)
+
+colwatjoin11<-full_join(colwatjoin10_noNAs, huc12perchaprewetmon, "HUC12") #Bio13
+colwatjoin11_noNAs <-na.omit(colwatjoin11)
+
+colwatjoin12<-full_join(colwatjoin11_noNAs, huc12perchapredrimon, "HUC12") #Bio14
+colwatjoin12_noNAs <-na.omit(colwatjoin12)
+
+colwatjoin13<-full_join(colwatjoin12_noNAs, huc12bacvel, "HUC12")
+colwatjoin13_noNAs <-na.omit(colwatjoin13)
+
+colwatjoin14<-full_join(colwatjoin13_noNAs, huc12difet, "HUC12")
+colwatjoin14_noNAs <-na.omit(colwatjoin14)
+
+colwatjoin15<-full_join(colwatjoin14_noNAs, huc12difhmi, "HUC12")
+colwatjoin15_noNAs <-na.omit(colwatjoin15)
+
+colwatjoin16<-full_join(colwatjoin15_noNAs, huc12difpnv, "HUC12")
+colwatjoin16_noNAs <-na.omit(colwatjoin16)
+
+colwatjoin17<-full_join(colwatjoin16_noNAs, huc12difswe, "HUC12")
+colwatjoin17_noNAs <-na.omit(colwatjoin17)
+
+colwatjoin18<-full_join(colwatjoin17_noNAs, huc12difsmc, "HUC12")
+colwatjoin18_noNAs <-na.omit(colwatjoin18)
+
+colwatjoin19<-full_join(colwatjoin18_noNAs, huc12junflo2040, "HUC12")
+colwatjoin19_noNAs<-na.omit(colwatjoin19)
+
+colwatjoin20<-full_join(colwatjoin19_noNAs, huc12strtemcha2040, "HUC12")
+colwatjoin20_noNAs<-na.omit(colwatjoin20)
+
+#vegetation#
+colwatjoin21<-full_join(colwatjoin20_noNAs, huc12meacanhei, "HUC12")
+colwatjoin21$MeanCanHei<-colwatjoin21$MeanCanHei%>% replace(is.na(.),0)
+colwatjoin21_noNAs <-na.omit(colwatjoin21)
+
+colwatjoin22<-full_join(colwatjoin21_noNAs, huc12nmripmarwetmear, "HUC12")
+colwatjoin22$PctMarWetMeaH12<-colwatjoin22$PctMarWetMeaH12%>% replace(is.na(.),0)
+colwatjoin22_noNAs <-na.omit(colwatjoin22)
+
+colwatjoin23<-full_join(colwatjoin22_noNAs, huc12nmripnatsemnatwoo, "HUC12")
+colwatjoin23$PctNatSemNatWoo12<-colwatjoin23$PctNatSemNatWoo12%>% replace(is.na(.),0)
+colwatjoin23_noNAs <-na.omit(colwatjoin23)
+
+colwatjoin24<-full_join(colwatjoin23_noNAs, huc12easbnatcov, "HUC12")
+colwatjoin24$PctNatCovSB<-colwatjoin24$PctNatCovSB%>% replace(is.na(.),0)
+colwatjoin24_noNAs <-na.omit(colwatjoin24)
+
+colwatjoin25<-full_join(colwatjoin24_noNAs, huc12easbtrecan, "HUC12")
+colwatjoin25$PctTreCanBuf<-colwatjoin25$PctTreCanBuf%>% replace(is.na(.),0)
+colwatjoin25_noNAs <-na.omit(colwatjoin25)
+
+colwatjoin26<-full_join(colwatjoin25_noNAs, huc12eafpshr, "HUC12")
+colwatjoin26_noNAs<-na.omit(colwatjoin26)
+
+colwatjoin27<-full_join(colwatjoin26_noNAs, huc12eafpwet, "HUC12")
+colwatjoin27_noNAs<-na.omit(colwatjoin27)
+
+#Soil/litho/topo#
+colwatjoin28<-full_join(colwatjoin27_noNAs, huc12meawatsto, "HUC12")
+colwatjoin28_noNAs<-na.omit(colwatjoin28)
+
+colwatjoin29<-full_join(colwatjoin28_noNAs, huc12meansbd, "HUC12")
+colwatjoin29_noNAs<-na.omit(colwatjoin29)
+
+colwatjoin30<-full_join(colwatjoin29_noNAs, huc12meancti, "HUC12")
+colwatjoin30_noNAs<-na.omit(colwatjoin30)
+
+colwatjoin31<-full_join(colwatjoin30_noNAs, huc12meancurve, "HUC12")
+colwatjoin31_noNAs<-na.omit(colwatjoin31)
+
+colwatjoin32<-full_join(colwatjoin31_noNAs, huc12meanelev, "HUC12")
+colwatjoin32_noNAs<-na.omit(colwatjoin32)
+
+colwatjoin33<-full_join(colwatjoin32_noNAs, huc12meantpi, "HUC12")
+colwatjoin33_noNAs<-na.omit(colwatjoin33)
+
+colwatjoin34<-full_join(colwatjoin33_noNAs, huc12meantwi, "HUC12")
+colwatjoin34_noNAs<-na.omit(colwatjoin34)
+
+colwatjoin35<-full_join(colwatjoin34_noNAs, huc12meanhli, "HUC12")
+colwatjoin35_noNAs<-na.omit(colwatjoin35)
+
+colwatjoin36<-full_join(colwatjoin35_noNAs, huc12meanaspectdiv, "HUC12")
+colwatjoin36_noNAs<-na.omit(colwatjoin36)
+
+colwatjoin37<-full_join(colwatjoin36_noNAs, huc12meangeomdiv, "HUC12")
+colwatjoin37_noNAs<-na.omit(colwatjoin37)
+
+colwatjoin38<-full_join(colwatjoin37_noNAs, huc12meanslopestd9, "HUC12")
+colwatjoin38_noNAs<-na.omit(colwatjoin38)
+
+colwatjoin39<-full_join(colwatjoin38_noNAs, huc8carbkarst, "HUC8")
+colwatjoin39$PctCarbKarstH8<-colwatjoin39$PctCarbKarstH8%>% replace(is.na(.),0)
+colwatjoin39_noNAs<-na.omit(colwatjoin39)
+
+colwatjoin40<-full_join(colwatjoin39_noNAs, huc8volckarst, "HUC8")
+colwatjoin40$PctVolcKarstH8<-colwatjoin40$PctVolcKarstH8%>% replace(is.na(.),0)
+colwatjoin40_noNAs<-na.omit(colwatjoin40)
+
+write.csv(colwatjoin40_noNAs, "ColdWaterJoin.csv", row.names=FALSE )
+names(colwatjoin40_noNAs)
+
+
+##Filter layer joins
+
+colwatfilter1<-full_join(colperhuc12s, HUC12fishes, "HUC12")
+colwatfilter1_noNAs<-na.omit(colwatfilter1)
+
+colwatfilter2<-full_join(colwatfilter1_noNAs, huc12eapascov, "HUC12")
+colwatfilter2_noNAs<-na.omit(colwatfilter2)
+
+colwatfilter3<-full_join(colwatfilter2_noNAs, huc12insdis, "HUC12")
+colwatfilter3_noNAs<-na.omit(colwatfilter3)
+
+colwatfilter4<-full_join(colwatfilter3_noNAs, huc12eafpdev, "HUC12")
+colwatfilter4_noNAs<-na.omit(colwatfilter4)
+
+colwatfilter5<-full_join(colwatfilter4_noNAs, huc12eafpagr, "HUC12")
+colwatfilter5_noNAs<-na.omit(colwatfilter5)
+
+colwatfilter6<-full_join(colwatfilter5_noNAs, huc12eafpdcro, "HUC12")
+colwatfilter6_noNAs<-na.omit(colwatfilter6)
+
+colwatfilter7<-full_join(colwatfilter6_noNAs, huc12devind4045, "HUC12")
+colwatfilter7_noNAs<-na.omit(colwatfilter7)
+
+colwatfilter8<-full_join(colwatfilter7_noNAs, huc12watquacha4045, "HUC12")
+colwatfilter8_noNAs<-na.omit(colwatfilter8)
+
+colwatfilter9<-full_join(colwatfilter8_noNAs, huc12wildfirechange, "HUC12")
+colwatfilter9_noNAs<-na.omit(colwatfilter9)
+
+colwatfilter10<-full_join(colwatfilter9_noNAs, huc12pctimp, "HUC12")
+colwatfilter10_noNAs<-na.omit(colwatfilter10)
+
+
+write.csv(colwatfilter10_noNAs, "ColdWaterFilters.csv", row.names=FALSE )
+names(colwatfilter10_noNAs)
+
+
+
+
+
+
---
+++ RefugiaLayers.R
@@ -0,0 +1,199 @@
+rm(list=ls(all=TRUE))
+setwd("E:/2023_CurrentNM_Work/R/Tables")
+library(tidyverse)
+
+
+###Base layers###
+huc12s<-read.csv("huc12s.csv") ##Springs/Ephemeral catchments/Amphibians and reptiles base layers##
+perstrhuc12s <- read.csv("perstrhuc12s.csv") ##Perennial streams base layer##
+colperhuc12s <- read.csv("colperhuc12s.csv") ##Cold-water perennial streams base layer##
+perlakponreshuc12s <- read.csv("perlakponreshuc12s.csv") ##Lakes, ponds, and reservoirs base layer##
+
+##Biotic value layers##
+huc12totaqusperic<-read.csv("huc12totaqusperic.csv") ##Total aquatic species richness - EnviroAtlas data
+huc12nataquvulind<-read.csv("huc12nataquvulind.csv") ##Native Aquatic Species Vulnerability Index - EnviroAtlas data
+huc12perwatherps<-read.csv("huc12perwatherpcount.csv" ) ##Species richness of snakes, amphs, and turtles that rely on per. water - IUCN and USGS data
+huc12ephcatamps<-read.csv("huc12ephcatampcount.csv" ) ##Species richness amphibians that rely on ephemeral catchments - USGS data
+HUC12fishes<-read.csv("huc12fishcount.csv" ) ##Species richness of fishes - IUCN data 
+#HUC12lakes ##?##
+#HUC12Springs ##?##
+
+##Climate layers##
+huc8diftemwetqua<-read.csv("huc8diftemwetqua.csv")##mean difference temperature wettest quarter Bio8
+huc8negdiftemwetqua<-read.csv("huc8negdiftemwetqua.csv") ##mean difference temperature wettest quarter *-1 
+huc8diftemdriqua<-read.csv("huc8difftemdriqua.csv")##mean difference temperature driest quarter Bio9
+huc8negdiftemdriquaa<-read.csv("huc8negdiftemdriqua.csv") ##mean difference temperature driest quarter *-1 
+huc8difanntem<-read.csv("huc8difanntem.csv")##Mean difference annual temperature Bio1
+huc8negdifanntem<-read.csv("huc8negdifanntem.csv") ##Mean difference annual temperature *-1
+huc8difmaxtemwarmon<-read.csv("huc8difmaxtemwarmon.csv")##Mean difference max temperature warmest month Bio5
+huc8negdifmaxtemwarmon<-read.csv("huc8negdifmaxtemwarmon.csv") ##Mean difference max temperature warmest month *-1
+huc8perchaprewarqua<-read.csv("huc8perchaprewarqua.csv") ##Mean percent change precipitation warmest quarter Bio18
+huc8perchaprecolqua<-read.csv("huc8perchaprecolqua.csv")  ##Mean percent change precipitation coldest quarter Bio19
+huc8perchaannpre<-read.csv("huc8perchaannpre.csv") ##Mean percent change annual precipitation Bio12
+huc8perchaprewetmon<-read.csv("huc8perchaprewetmon.csv") ##Mean percent change precipitation wettest month Bio13
+huc8perchapredrimon<-read.csv("huc8perchapredrimon.csv") ##Mean percent change precipitation driest month Bio14
+huc8difet<-read.csv("huc8difet.csv")##Mean difference evapotranspiration 
+huc8negdifet<-read.csv("huc8negdifet.csv") ##Mean difference evapotranspiration *-1
+huc8difhmi<-read.csv("huc8difhmi.csv")##Mean difference heat moisture index
+huc8negdifhmi<-read.csv("huc8negdifhmi.csv") ##Mean difference heat moisture index *-1
+huc8difpnv<-read.csv("huc8difpnv.csv")##Mean difference potential evapotranspiration of natural vegetation
+huc8negdifpnv<-read.csv("huc8negdifpnv.csv") ##Mean difference potential evapotranspiration of natural vegetation *-1
+
+huc8difsmc<-read.csv("huc8difsmc.csv") ##Mean difference soil moisture content
+huc8difswe<-read.csv("huc8difswe.csv") ##Mean difference snow water equivalent
+
+huc12diftemwetqua<-read.csv("huc12difftempwetqua.csv") ##mean difference temperature wettest quarter Bio8
+huc12negdiftempwetqua<-read.csv("huc12negdifftempwetqua.csv") ##mean difference temperature wettest quarter *-1
+huc12diftemdriqua<-read.csv("huc12difftempdriqua.csv") ##mean difference temperature driest quarter Bio9
+huc12negdiftempdriqua<-read.csv("huc12negdifftempdriqua.csv") ##mean difference temperature driest quarter *-1 
+huc12difanntem<-read.csv("huc12diffanntemp.csv") ##Mean difference annual temperature Bio1
+huc12negdifanntem <-read.csv("huc12negdiffanntemp.csv") ##Mean difference annual temperature *-1
+huc12difmaxtemwarmon<-read.csv("huc12diffmaxtemwarmon.csv")##Mean difference max temperature warmest month Bio5
+huc12negdifmaxtemwarmon<-read.csv("huc12negdiffmaxtemwarmon.csv") ##Mean difference max temperature warmest month *-1
+huc12diftempran<-read.csv("huc12difftempran.csv")##Mean difference temperature range Bio7
+huc12negdiftempran<-read.csv("huc12negdifftempran.csv") ##Mean difference temperature range *-1
+huc12diftemsea<-read.csv("huc12difftemsea.csv")##Mean difference temperature seasonality Bio4
+huc12negdiftemsea<-read.csv("huc12negdifftemsea.csv") ##Mean difference temperature seasonality *-1
+huc12difiso<-read.csv("huc12difiso.csv")##Mean difference isothermality Bio3
+huc12negdifiso<-read.csv("huc12negdifiso.csv") ##Mean difference isothermality *-1
+huc12difmeadiuran<-read.csv("huc12diffmeadiuran.csv") ##Mean difference diurnal range Bio2
+huc12negdifmeadiuran<-read.csv("huc12negdiffmeadiuran.csv") ##Mean difference diurnal range *-1
+huc12pctchaprewarqua<-read.csv("huc12pctchaprewarqua.csv") ##Mean percent change precipitation warmest quarter Bio18
+huc12perchaprecolqua<-read.csv("huc12perchaprecolqua.csv") ##Mean percent change precipitation coldest quarter Bio19
+huc12perchaannpre<-read.csv("huc12perchaannpre.csv")  ##Mean percent change annual precipitation Bio12
+huc12perchaprewetmon<-read.csv("huc12perchaprewetmon.csv") ##Mean percent change precipitation wettest month Bio13
+huc12perchapredrimon<-read.csv("huc12perchapredrimon.csv") ##Mean percent change precipitation driest month Bio14
+huc12forvel<-read.csv("huc12forvel.csv")##Mean forward climate velocity
+huc12negforvel<-read.csv("huc12negforvel.csv") ##Mean forward climate velocity *-1
+huc12bacvel<-read.csv("huc12bacvel.csv")##Mean backward climate velocity
+huc12negbacvel<-read.csv("huc12negbacvel.csv") ##Mean backward climate velocity *-1
+huc12dissim<-read.csv("huc12dissim.csv") ##Mean climate dissimilarity 
+huc12negdissim<-read.csv("huc12negdissim.csv") ##Mean climate dissimilarity *-1
+huc12novelty<-read.csv("huc12novelty.csv") ##Mean climate novelty
+huc12negnovelty<-read.csv("huc12negnovelty.csv") ##Mean climate novelty *-1
+huc12noveltysd<-read.csv("huc12noveltysd.csv") ##Std climate novelty
+huc12difet<-read.csv("huc12difet.csv") ##Difference in evapotranspiration
+huc12negdifet<-read.csv("huc12difhmi.csv") ##Difference in evapotranspiration *-1
+huc12difhmi<-read.csv("huc12difhmi.csv") ##Difference in heat moisture index
+huc12negdifhmi<-read.csv("huc12negdifhmi.csv") ##Difference in heat moisture index *-1
+huc12difpnv<-read.csv("huc12difpnv.csv") ##Difference in PET of natural vegetation
+huc12negdifpnv<-read.csv("huc12negdifpnv.csv") ##Difference in PET of natural vegetation *-1
+huc12difsmc<-read.csv("huc12difsmc.csv") ##Difference in soil moisture content
+huc12difswe<-read.csv("huc12difswe.csv") ##Difference snow water equivalent
+
+
+##Soil layers##
+huc12soilhydgroweldra<-read.csv("huc12soilhydgroweldra.csv") ##pct. with well-drained soils
+huc8soilhydgroweldra<-read.csv("huc8soilhydgroweldra.csv") ##pct. with well-drained soils
+huc12soilhydgrosloinf<-read.csv("huc12soilhydgrosloinf.csv") ##pct. slow-infiltrating soils
+huc8soilhydgrosloinf<-read.csv("huc8soilhydgrosloinf.csv") ##pct. slow-infiltrating soils
+huc8negsoibulden<-read.csv("huc8negsoibulden.csv") ##soil bulk density *-1
+huc12negsoibulden<-read.csv("huc12negsoibulden.csv") ##soil bulk density 
+huc12soibulden <-read.csv("huc12soibulden.csv") ##soil bulk density
+huc8soibulden<-read.csv("huc8soibulden.csv") ##soil bulk density
+huc8soilhydgrorich<-read.csv("huc8soilhydgrorich.csv") ##number of soil hydrogroups
+huc12soilhydgrorich<-read.csv("huc12soilhydgrorich.csv") ##number of soil hydrogroups
+huc12minwattab<-read.csv("huc12minwattab.csv") ## minimum water table height
+huc12meawatsto<-read.csv("huc12meawatsto.csv") ##Mean soil water storage
+huc12meansbd<-read.csv("huc12meansbd.csv") ##mean soil bulk density in huc12s
+
+##Lithology layers##
+huc8carbkarst<-read.csv("huc8carbkarst.csv") #pct with carbonate karst
+huc8volckarst<-read.csv("huc8volckarst.csv") #pct with volcanic karst
+huc8uncon<-read.csv("huc8uncon.csv") #pct with unconsolidated aquifers
+huc8aquirich<-read.csv("huc8aquirich.csv") #number of underlying aquifers
+huc12carbkarst<-read.csv("huc12carbkarst.csv") #pct with carbonate karst
+huc12volckarst<-read.csv("huc12volckarst.csv") #pct with volcanic karst
+huc12uncon<-read.csv("huc12uncon.csv") #pct with unconsolidated aquifers
+huc12aquirich<-read.csv("huc12aquirich.csv") #number of underlying aquifers
+
+##Topography layers##
+huc12meanaspectdiv<-read.csv("huc12meanaspectdiv.csv") ##mean aspect diversity
+huc12meancti<-read.csv("huc12meancti.csv") ##mean compond topographic index
+huc12meancurve<-read.csv("huc12meancurve.csv") ##mean earth curvature##
+huc12meanelev<-read.csv("huc12meanelev.csv") ##Mean elevation##
+huc12meanelevdiv<-read.csv("huc12meanelevdiv.csv") ##Mean elevation diversity##
+huc12meangeomdiv<-read.csv("huc12stdgeomdiv.csv") ##Mean geomorphologic diversity##
+huc12meanhli<-read.csv("huc12meanhli.csv") ##Mean heat load index##
+huc12pctnorth<-read.csv("huc12pctnorth.csv") ##pct. northness##
+huc12meanrough<-read.csv("huc12meanrough.csv") ##mean roughness##
+huc12meanslopestd3<-read.csv("huc12meanslopestd3.csv") ##mean slope std 3x3 cell area##
+huc12meanslopestd9<-read.csv("huc12meanslopestd9.csv")  ##mean slope std 9x9 cell area##
+huc12meantpi<-read.csv("huc12meantpi.csv") ##mean topographic position index##
+huc12meantwi<-read.csv("huc12meantwi.csv") ##mean topographic wetness index##
+huc12meanvrm<-read.csv("huc12meanvrm.csv") ##mean vector ruggedness index?##
+
+huc12meanhlidiv<-read.csv("huc12meanhlidiv.csv")
+huc12stdhlidiv<-read.csv("huc12stdhlidiv.csv")
+huc12stdelev<-read.csv("huc12stdelev.csv")
+huc12stdrough<-read.csv("huc12stdrough.csv")
+huc12stdvrm<-read.csv("huc12stdvrm.csv")
+
+##Hydrology layers##
+huc12wateryield<-read.csv("huc12wateryield.csv") ##water yield
+huc12watbodare<-read.csv("huc12watbodare.csv") ##water body area
+huc12strden<-read.csv("huc12strden.csv") ##stream density
+huc12sprden<-read.csv("huc12sprden.csv")  ##spring density
+huc12strtem10<-read.csv("huc12strtem10.csv") ##total miles of streams with mean Aug temps 10 C or less
+huc12pctper<-read.csv("huc12pctper.csv")##percent of  stream miles that are perennial
+huc12pct10deg<-read.csv("huc12pct10deg.csv") ##percent of stream miles with mean august water temps 10 degrees or less
+huc12pct17deg<-read.csv("huc12pct17deg.csv") ##percent of stream miles with mean august water temps 10 degrees or less
+huc12plaintlak<-read.csv("huc12plaintlak.csv")
+
+##Vegetation layers##
+huc12meacanhei<-read.csv("huc12meacanhei.csv") ##mean canopy height
+huc8nmripmarwetmear<-read.csv("huc8nmripmarwetmear.csv") ##pct with wet meadow riparian vegetation
+huc12nmripmarwetmear<-read.csv("huc12nmripmarwetmear.csv") ##pct with wet meadow riparian vegetation
+huc8nmripnatsemnatwoo<-read.csv("huc8nmripnatsemnatwoo.csv") ##pct with woody riparian vegetation
+huc12nmripnatsemnatwoo<-read.csv("huc12nmripnatsemnatwoo.csv") ##pct with woody riparian vegetation
+huc12f2fnatcov<-read.csv("huc12f2fnatcov.csv") ##pct with natural cover
+huc12f2fripnatcov<-read.csv("huc12f2fripnatcov.csv") ##pct with riparian natural cover
+huc12eafpnat<-read.csv("huc12eafpnat.csv") ##pct. natural floodplain cover
+huc12eafpfor<-read.csv("huc12eafpfor.csv") ##pct. forested floodplain cover
+huc12eafpshr<-read.csv("huc12eafpshr.csv") ##pct. shrubby floodplain cover
+huc12eafpwet<-read.csv("huc12eafpwet.csv") ##pct. wetland floodplain cover
+huc12eawetcov<-read.csv("huc12eawetcov.csv") ##pct. wetland  cover
+huc12eaforwoowetcov<-read.csv("huc12eaforwoowetcov.csv") ##pct. forest or wetland cover
+huc12eaheremwetcov<-read.csv("huc12eaheremwetcov.csv") ##pct. emergent wetland cover
+huc12easbforwoowet<-read.csv("huc12easbforwoowet.csv") ##pct. stream buffer with forest or wetland cover
+huc12easbfor<-read.csv("huc12easbfor.csv") ##pct. stream buffer with forest  cover
+huc12easbnatcov<-read.csv("huc12easbnatcov.csv") ##pct. stream buffer natural cover
+huc12easbtrecan<-read.csv("huc12easbtrecan.csv") ##pct. stream buffer with tree canopy
+huc12corridors<-read.csv("huc12corridors.csv") ##total number of corridors##
+
+##Future change layers##
+huc12meaanflo2040<-read.csv("huc12meaanflo2040.csv") ##Change in mean annual flow
+huc121_5yrflo2040<-read.csv("huc121_5yrflo2040.csv") ##Change in 5 year peak flow
+huc1210yrflo2040<-read.csv("huc1210yrflo2040.csv") ##Change in 10 year peak flow
+huc1225yrflo2040<-read.csv("huc1225yrflo2040.csv") ##Change in 25 year peak flow
+huc12lowflodat2040<-read.csv("huc12lowflodat2040.csv") ##Change in low flow date
+huc12lowflovol2040<-read.csv("huc12lowflovol2040.csv") ##change in low flow volume
+huc12basfloind2040<-read.csv("huc12basfloind2040.csv") ##Change in base flow index
+huc12winflo2040<-read.csv("huc12winflo2040.csv") ##Change in winter low flow
+huc12junflo2040<-read.csv("huc12junflo2040.csv") ##Change in mean June flow
+huc12sumflo2040<-read.csv("huc12sumflo2040.csv") ##Change in mean Jun, Jul, Aug flow
+huc12strtemcha2040<-read.csv("huc12strtemcha2040.csv") ##Mean August stream temperature change
+huc12negstrtemcha2040<-read.csv("huc12negstrtemcha2040.csv") ##Negative mean August stream temperature change
+
+
+##Disturbance layers
+huc12agslope3<-read.csv("huc12agslope3.csv") ##Agriculture cover on 3 degree slopes
+huc12paslope3<-read.csv("huc12paslope3.csv") ##Pasture cover on 3 degree slopes
+huc12crslope3<-read.csv("huc12crslope3.csv") ##Crop cover on 3 degree slopes
+huc12agslope9<-read.csv("huc12agslope9.csv") ##Agriculture cover on 9 degree slopes
+huc12paslope9<-read.csv("huc12paslope9.csv") ##Pasture cover on 9 degree slopes
+huc12crslope9<-read.csv("huc12crslope9.csv") ##Crop cover on 9 degree slopes
+huc12eadevcov<-read.csv("huc12eadevcov.csv") ##Developed land cover
+huc12eaagcov<-read.csv("huc12eaagcov.csv")  ##Agriculture land cover
+huc12eapascov<-read.csv("huc12eapascov.csv") ##Pasture cover
+huc12eacrocov<-read.csv("huc12eacrocov.csv") ##Crop cover
+huc12insdis<-read.csv("huc12insdis.csv") ##Insect and disease risk
+huc12eafpdev<-read.csv("huc12eafpdev.csv") ##Floodplain development
+huc12eafpagr<-read.csv("huc12eafpagr.csv") ##Floodplain agriculture cover
+huc12eafpdcro<-read.csv("huc12eafpdcro.csv") ##Floodplain crop cover
+huc12pctimp<-read.csv("huc12pctimp.csv") ##Pct. stream miles with impairments (pollution, etc.) 
+huc12devind4045<-read.csv("huc12devind4045.csv") ##Development index for 2040 under rcp.45 scenario
+huc12devind4085<-read.csv("huc12devind4085.csv") ##Development index for 2040 under rcp.85 scenario
+huc12watquacha4045<-read.csv("huc12watquacha4045.csv") ##Projected change in water quantity under rcp.45 scenario
+huc12watquacha4085<-read.csv("huc12watquacha4085.csv") ##Projected change in water quantity under rcp.85 scenario
+huc12wildfirechange<-read.csv("huc12wildfirechange.csv") ##Projected wildfire risk
